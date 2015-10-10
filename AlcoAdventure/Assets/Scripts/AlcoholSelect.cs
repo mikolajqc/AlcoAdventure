2922179a1e39f3f7786256f85268e833e0fb429e
mikolaj <mikolajqc@gmail.com>
2015-10-10 15:35:52 +0200

    volume and alcohol updating

---
+++ AlcoAdventure/Assets/Scripts/AlcoholSelect.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AlcoholSelect : MonoBehaviour {
+
+    public float BUTTONWIDTH = Screen.width/5;
+    public float BUTTONHEIGHT = Screen.height/4;
+    public float BREAKEWIDTH = Screen.width / 10;
+    public float BREAKEHEIGHT = Screen.height / 16;
+
+    public enum AlcoholChoice
+    {
+        BEER,
+        WINE,
+        CHAMPAGNE,
+        VODKA,
+        SPIRIT,
+        DENATURATE,
+    };
+    AlcoholChoice alcoholChoice = 0;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CheckResolution();
+
+    }
+
+    //checking resolution in real time
+    void CheckResolution()
+    {
+    BUTTONWIDTH = Screen.width / 5;
+    BUTTONHEIGHT = Screen.height / 4;
+    BREAKEWIDTH = Screen.width / 10;
+    BREAKEHEIGHT = Screen.height / 16;
+}
+
+    void OnGUI()
+    {
+        //enum alkohol choice
+
+
+        
+
+
+        //Alcohol MENU
+        if (GUI.Button(new Rect(BREAKEWIDTH, BREAKEHEIGHT, BUTTONWIDTH, BUTTONHEIGHT), "Beer"))
+        {
+            alcoholChoice = AlcoholChoice.BEER;
+            Application.LoadLevel("VolumeSelect");
+        }
+        if (GUI.Button(new Rect(2 * BREAKEWIDTH + BUTTONWIDTH, BREAKEHEIGHT, BUTTONWIDTH, BUTTONHEIGHT), "Wine"))
+        {
+            alcoholChoice = AlcoholChoice.WINE;
+            Application.LoadLevel("VolumeSelect");
+        }
+        if (GUI.Button(new Rect(3 * BREAKEWIDTH + 2 * BUTTONWIDTH, BREAKEHEIGHT, BUTTONWIDTH, BUTTONHEIGHT), "Champagne"))
+        {
+            alcoholChoice = AlcoholChoice.CHAMPAGNE;
+            Application.LoadLevel("VolumeSelect");
+        }
+        if (GUI.Button(new Rect(BREAKEWIDTH, 2 * BREAKEHEIGHT + BUTTONHEIGHT, BUTTONWIDTH, BUTTONHEIGHT), "Wodka"))
+        {
+            alcoholChoice = AlcoholChoice.VODKA;
+            Application.LoadLevel("VolumeSelect");
+        }
+        if (GUI.Button(new Rect(2 * BREAKEWIDTH + BUTTONWIDTH, 2 * BREAKEHEIGHT + BUTTONHEIGHT, BUTTONWIDTH, BUTTONHEIGHT), "Spirit"))
+        {
+            alcoholChoice = AlcoholChoice.SPIRIT;
+            Application.LoadLevel("VolumeSelect");
+        }
+        if (GUI.Button(new Rect(3 * BREAKEWIDTH + 2 * BUTTONWIDTH, 2 * BREAKEHEIGHT + BUTTONHEIGHT, BUTTONWIDTH, BUTTONHEIGHT), "Denaturate"))
+        {
+            alcoholChoice = AlcoholChoice.DENATURATE;
+            Application.LoadLevel("VolumeSelect");
+        }
+        if (GUI.Button(new Rect(BREAKEWIDTH, 3 * BREAKEHEIGHT + 2* BUTTONHEIGHT, 3*BUTTONWIDTH + 2*BREAKEWIDTH, BUTTONHEIGHT/2), "Back"))
+        {
+            //Back
+        }
+
+        //sending kind of alco to VolumeSelect;
+        VolumeSelect.alcoholSelected = (int)alcoholChoice;
+
+    }
+}
