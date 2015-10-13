1c689973907b9fb622974e2987a36cdc3a277e23
mikolaj <mikolajqc@gmail.com>
2015-10-13 12:48:33 +0200

    finished graph without axises

--- AlcoAdventure/Assets/Scripts/Graph.cs
+++ AlcoAdventure/Assets/Scripts/Graph.cs
@@ -10,9 +10,12 @@ public class Graph : MonoBehaviour
     public float graphWidth = 2;
     public float yDivider = 1;
     public float max = 3;
+    public Color[] colors;
+
+    public float[] borders = new float[5];
 
 
-    public float[] actualColorRGB = new float[3];
+    //public float[] actualColorRGB = new float[3];
     public int resolution = 300;
 
     //this variable is connect with sober time +10% *h 
@@ -30,39 +33,55 @@ public class Graph : MonoBehaviour
     {
         points = new ParticleSystem.Particle[resolution];
 
-        float incrementX = graphWidth / (resolution - 1); //1/299
-        float incrementY = graphLenght / (resolution - 1); //1/299
+        float incrementScreen = graphWidth / (resolution - 1); //1/299
+        float incrementFunction = graphLenght / (resolution - 1); //1/299
         CalculateYDivider();
         
-
-        float x1,x2;
+        // x and y are real parameters of function y=f(x)
+        // x1 and y1 are positions of pointers on graph
+        float x1,x, y, y1;
 
         for (int i = 0; i < resolution; ++i)
             {
                 //prepared variables: x1 is variable destribing position x of point, x2 is variable used in function of point y position
-                x1 = i * incrementX;
-                x2 = i * incrementY;
+                x1 = i * incrementScreen;
+                x = i * incrementFunction;
+
+                y = (Mathf.Sin(x))*5;
+                y1 = y/ yDivider;
 
                 //Here is function - z is coordinate = f(x)!
-                points[i].position = new Vector3((x1 ), 0f,(Mathf.Ceil(x2))/yDivider);
+                //Function have to be divide by yDivider to scale it to graphHeight
+                points[i].position = new Vector3(x1 , 0f, y1);
 
-                //Changing color of graph:
-                points[i].color = new Color(x1, 255-x1, 0);
-                
-                //Size of graph:
-                points[i].size = 0.1f;
+            //Changing color of graph:
+            CalculateColor(i, y);
+
+            //Size of graph:
+            points[i].size = 0.1f;
             }
     }
 
-    void CalculateColor()
+    void CalculateColor(int i, float y)
     {
-
+        //i is a number of point in points
+           for (int k = 0; k < borders.Length; ++k)
+            {
+                if(y<=borders[k])
+                {
+                    points[i].color = colors[k];
+                    break;
+                }
+                else
+                {
+                    points[i].color = colors[5];
+                }
+            }
     }
 
     void Update()
     {
         GetComponent<ParticleSystem>().SetParticles(points, points.Length);
-        print(graphWidth);
     }
 
     void CalculateMax()
